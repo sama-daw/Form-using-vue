361c80ce6525a92c4f6cccd33576b08368b1faed
SamaDaw <samadaw3@gmail.com>
2025-09-17 06:47:43 +0300

    solved a problem

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,6 @@ body {
   margin: 0;
   min-height: 100vh;
   background: linear-gradient(135deg, #181818 0%, #232526 100%);
-  /* تدرج بين الرمادي الداكن والأسود لتناسب الذهبي */
   font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true
-})
-module.exports = {
-  publicPath: '/Form-using-vue/'
-};
+  transpileDependencies: true,
+  publicPath: '/Form-using-vue/'  
+});
